4cea718a84f784043d894d4a26e80c9244a32798
kiwi <5103662780@163.com>
2017-05-05 00:18:18 +0800

    last

--- app/src/main/java/com/example/wechat/MainActivity.java
+++ app/src/main/java/com/example/wechat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wechat;
 
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         initViews();
+        initEvents();
     }
 
     private void initViews() {
@@ -43,5 +45,44 @@ public class MainActivity extends AppCompatActivity {
         two.setIcon(R.drawable.ic_contact);
         three.setIcon(R.drawable.ic_discovery);
         four.setIcon(R.drawable.ic_mine);
+    }
+
+    private void initEvents() {
+        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab == mTabLayout.getTabAt(0)) {
+                    one.setIcon(R.drawable.ic_wechat_s);
+                    mViewPager.setCurrentItem(0);
+                } else if (tab == mTabLayout.getTabAt(1)) {
+                    two.setIcon(R.drawable.ic_contact_s);
+                    mViewPager.setCurrentItem(1);
+                } else if (tab == mTabLayout.getTabAt(2)) {
+                    three.setIcon(R.drawable.ic_discovery_s);
+                    mViewPager.setCurrentItem(2);
+                } else if (tab == mTabLayout.getTabAt(3)) {
+                    four.setIcon(R.drawable.ic_mine_s);
+                    mViewPager.setCurrentItem(3);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                if (tab == mTabLayout.getTabAt(0)) {
+                    one.setIcon(R.drawable.ic_wechat);
+                } else if (tab == mTabLayout.getTabAt(1)) {
+                    two.setIcon(R.drawable.ic_contact);
+                } else if (tab == mTabLayout.getTabAt(2)) {
+                    three.setIcon(R.drawable.ic_discovery);
+                }else if (tab == mTabLayout.getTabAt(3)){
+                    four.setIcon(R.drawable.ic_mine);
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
